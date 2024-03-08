ce4e66d7f923932f8bd7b257f215290ac6433c8b
Daeho <dooho199234@nate.com>
2024-03-08 10:04:30 +0900

    Lv2

--- calculator.playground/Pages/Lv2.xcplaygroundpage/Contents.swift
+++ calculator.playground/Pages/Lv2.xcplaygroundpage/Contents.swift
@@ -2,3 +2,46 @@ import UIKit
 
 // 내배캠 1주차 과제 - 계산기 Lv2
 // 이대호
+
+
+import Foundation
+
+class Calculator {
+    
+    //    사칙연산을 계산하는 메서드
+    func calculator(_ operatorSymbol: String, firstNumber: Double, secondNumber: Double) -> Double {
+        var result: Double = 0.0
+        
+        switch operatorSymbol {
+        case "+":
+            result = firstNumber + secondNumber
+        case "-":
+            result = firstNumber - secondNumber
+        case "*":
+            result = firstNumber * secondNumber
+        case "/":
+            if secondNumber != 0 {
+                result = firstNumber / secondNumber
+            }else {
+                print("ERROR")
+            }
+        case "%":
+            result = Double(Int(firstNumber) % Int(secondNumber))
+        default:
+            print("사칙 연산이 아닙니다.")
+            break
+        }
+        
+        return result
+    }
+    
+    
+}
+
+
+let calculator = Calculator()
+print("더하기 : \(calculator.calculator("+", firstNumber: 10, secondNumber: 3))")
+print("뺄셈 : \(calculator.calculator("-", firstNumber: 10, secondNumber: 5))")
+print("곱셈 : \(calculator.calculator("*", firstNumber: 10, secondNumber: 3))")
+print("나눗셈: \(calculator.calculator("/", firstNumber: 10, secondNumber: 5))")
+print("나머지: \(calculator.calculator("%", firstNumber: 10, secondNumber: 3))")
